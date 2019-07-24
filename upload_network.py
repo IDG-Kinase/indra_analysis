9392cc9e35b3a89612748009d7f28d8e96df859a
Ben Gyori <ben.gyori@gmail.com>
2019-07-24 15:58:40 -0400

    Add script to upload a network to ndex

---
+++ upload_network.py
@@ -0,0 +1,16 @@
+import sys
+import pickle
+from indra.databases import ndex_client
+from indra.assemblers.cx import CxAssembler
+from indra.assemblers.cx.semantic_hub import add_semantic_hub_layout
+
+
+if __name__ == '__main__':
+    kinase = sys.argv[1]
+    with open('dark_kinase_statements_assembled.pkl', 'rb') as fh:
+        stmts = pickle.load(fh)
+    cxa = CxAssembler(stmts[kinase])
+    cxa.make_model()
+    add_semantic_hub_layout(cxa.cx, kinase)
+    model_id = cxa.upload_model()
+    ndex_client.set_style(model_id)
